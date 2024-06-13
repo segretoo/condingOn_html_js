54ecc3d0d8922f3ed45466f57b6e61229a649f42
segretoo <segreto8867@gamil.com>
2024-06-13 20:55:02 +0900

    ef

---
+++ js/charge.js
@@ -0,0 +1,18 @@
+let age = prompt("나이를 입력해주세요");
+let charge = 0;
+
+if (age < 8){
+    document.write("미취학 아동입니다 <br>");
+    charge = 1000;
+}else if(age >= 8 && age < 14){
+    document.write("초등학생입니다<br>")
+    charge = 2000;
+}else if(age >= 14 && age < 20){
+    document.write("중, 고등학생입니다<br>")
+    charge = 2500;
+}else{
+    document.write("성인입니다<br>")
+    charge = 3000;
+}
+
+document.write("입장료는<span>" + charge + "<span>원 입니다")
